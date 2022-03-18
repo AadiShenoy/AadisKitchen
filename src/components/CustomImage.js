834464ccce0421c3da34f97c5a8bf70c6260b363
Adithya Shenoy <adithya.shenoy@ymedialabs.com>
2022-03-18 09:54:03 +0530

    Refactored

--- src/components/CustomImage.js
+++ src/components/CustomImage.js
@@ -1,7 +1,25 @@
-export default function CustomImage({imgSrc, pt}){
-    return (
-        <div className="custom-image" style={{paddingTop: pt}}>
-            <img src={imgSrc} alt="" />
-        </div>
-    )
+import { useState, useCallback } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function CustomImage({ imgSrc, pt }) {
+  const [load, setLoad] = useState(false);
+  const onLoad = useCallback(() => {
+    setLoad(true);
+  }, []);
+  return (
+    <div className="custom-image" style={{ paddingTop: pt }}>
+      <img
+        className="img"
+        src={imgSrc}
+        alt=""
+        onLoad={onLoad}
+        style={{ display: load ? "block" : "none" }}
+      />
+      {!load && (
+        <CircularProgress
+          className="img loader"
+        />
+      )}
+    </div>
+  );
 }
--- src/pages/RecipeDetail.js
+++ src/pages/RecipeDetail.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
 function RecipeDetail() {
-    const location = useLocation()
-  const { recipe } = location.state
+  const location = useLocation();
+  const { recipe } = location.state;
   return (
     <div>
-        <h2>{recipe.title}</h2>
-        <img src={recipe.image} style={{width:'250px',height:'250px',marginTop:'10px'}} alt="food"/>
-        <h3 style={{marginTop:"10px"}}>Ingredients</h3>
-        <p style={{marginTop:"10px"}}>
-            {recipe.ingredients}
-        </p>
-        <h3 style={{marginTop:"10px"}}>Instructions</h3>
-        <p style={{marginTop:"10px"}}>
-           {recipe.instructions}
-        </p>
+      <h2>{recipe.title}</h2>
+      <img
+        src={recipe.image}
+        style={{
+          width: "250px",
+          height: "250px",
+          marginTop: "10px",
+          borderRadius: "5px",
+        }}
+        alt="food"
+      />
+      <h3 style={{ marginTop: "10px" }}>Ingredients</h3>
+      <p style={{ marginTop: "10px" }}>{recipe.ingredients}</p>
+      <h3 style={{ marginTop: "10px" }}>Instructions</h3>
+      <p style={{ marginTop: "10px" }}>{recipe.instructions}</p>
     </div>
-  )
+  );
 }
 
-export default RecipeDetail
+export default RecipeDetail;
