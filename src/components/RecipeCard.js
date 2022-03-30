fffb72fb0769a9846751e396f23d901dce6b72d9
Adithya Shenoy <adithya.shenoy@ymedialabs.com>
2022-03-30 15:35:33 +0530

    Food recipe app refactored

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -5,27 +5,80 @@ import "aos/dist/aos.css";
 import veg from "../assets/veg.png";
 import nonveg from "../assets/nonveg.png";
 import { Link } from "react-router-dom";
+import moment from "moment";
 export default function RecipeCard({ recipe }) {
   useEffect(() => {
     Aos.init({ duration: 700 });
   }, []);
+
+  var date = new Date(Date.now());
+  var result =
+    date.getMonth() > 8
+      ? date.getMonth() + 1
+      : "0" +
+        (date.getMonth() + 1) +
+        "/" +
+        (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+        "/" +
+        date.getFullYear();
+
+  var a = moment(result, "M/D/YYYY");
+  var b = moment(recipe.date, "M/D/YYYY");
+  var diffDays = a.diff(b, "days");
+  console.log(parseInt(diffDays / 365));
+  if (parseInt(diffDays / 365) > 0) {
+    result = parseInt(diffDays / 365);
+    if (result === 1) {
+      result = result + " year ago";
+    } else {
+      result = result + " years ago";
+    }
+  } else if (parseInt(diffDays / 30) > 0) {
+    result = parseInt(diffDays / 30);
+    if (result === 1) {
+      result = result + " month ago";
+    } else {
+      result = result + " months ago";
+    }
+  } else {
+    result = diffDays;
+    if (result === 0) {
+      result = " Today";
+    } else if (result === 1) {
+      result = result + " day ago";
+    } else {
+      result = result + " days ago";
+    }
+  }
 
   return (
     <div className="recipe-card" data-aos="zoom-in" data-aos-offset="100">
       <CustomImage imgSrc={recipe.image} pt="75%" />
       <div className="recipe-card-info">
         <p className="recipe-title">{recipe.title}</p>
-        <div style={{display:'flex',flexDirection:'row'}}>
-        <img
-          className="category"
-          src={recipe.category === "veg" ? veg : nonveg}
-          alt=""
-        />
-        <p className="category-text">{recipe.category==="veg"?"Veg":"Non Veg"}</p>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <img
+            className="category"
+            src={recipe.category === "veg" ? veg : nonveg}
+            alt=""
+          />
+          <p className="category-text">
+            {recipe.category === "veg" ? "Veg" : "Non Veg"}
+          </p>
         </div>
-        <Link className="view-btn" to='/detail' state={{recipe:recipe}}>
-          VIEW RECIPE
-        </Link>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: "0.5em",
+          }}
+        >
+          <Link className="view-btn" to="/detail" state={{ recipe: recipe }}>
+            VIEW RECIPE
+          </Link>
+          <p className="date">{result}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/RecipeDetail.js
+++ src/pages/RecipeDetail.js
@@ -4,29 +4,27 @@ function RecipeDetail() {
   const location = useLocation();
   const { recipe } = location.state;
   return (
-    <>
-      <div>
-        <h2>{recipe.title}</h2>
-        <img
-          src={recipe.image}
-          style={{
-            width: "250px",
-            height: "250px",
-            marginTop: "10px",
-            borderRadius: "5px",
-          }}
-          alt="food"
-        />
-        <h3 style={{ marginTop: "10px" }}>Ingredients</h3>
-        <p style={{ marginTop: "10px", whiteSpace: "pre-line" }}>
-          {recipe.ingredients}
-        </p>
-        <h3 style={{ marginTop: "10px" }}>Instructions</h3>
-        <p style={{ marginTop: "10px", whiteSpace: "pre-line" }}>
-          {recipe.instructions}
-        </p>
-      </div>
-    </>
+    <div style={{ backgroundColor: "#eff4f7", padding: "10px" }}>
+      <h2>{recipe.title}</h2>
+      <img
+        src={recipe.image}
+        style={{
+          width: "250px",
+          height: "250px",
+          marginTop: "10px",
+          borderRadius: "5px",
+        }}
+        alt="food"
+      />
+      <h3 style={{ marginTop: "10px" }}>Ingredients</h3>
+      <p style={{ marginTop: "10px", whiteSpace: "pre-line" }}>
+        {recipe.ingredients}
+      </p>
+      <h3 style={{ marginTop: "10px" }}>Instructions</h3>
+      <p style={{ marginTop: "10px", whiteSpace: "pre-line" }}>
+        {recipe.instructions}
+      </p>
+    </div>
   );
 }
 
