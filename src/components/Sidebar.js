191988cd66ef423f87de306a851bcc14103ed903
Adithya Shenoy <adithya.shenoy@ymedialabs.com>
2022-10-03 16:59:13 +0530

    Added:outside click to close sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,40 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link, useLocation } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Sidebar({links, close}){
-    const location = useLocation()
-    return (
-        <div className="sidebar" onClick={close}>
-            { links.map(link => (
-                <Link to={link.path} className={location.pathname === link.path ? "sidebar-link active" : "sidebar-link"} key={link.name}>
-                    <FontAwesomeIcon icon={link.icon} />
-                    {link.name}
-                </Link>
-            )) }
-        </div>
-    )
+import { useEffect, useRef } from "react";
+
+export default function Sidebar({ links, close }) {
+  const location = useLocation();
+
+  let sideBarRef = useRef();
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!sideBarRef.current.contains(e.target)) {
+        close();
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+  return (
+    <div className="sidebar" onClick={close} ref={sideBarRef}>
+      {links.map((link) => (
+        <Link
+          to={link.path}
+          className={
+            location.pathname === link.path
+              ? "sidebar-link active"
+              : "sidebar-link"
+          }
+          key={link.name}
+        >
+          <FontAwesomeIcon icon={link.icon} />
+          {link.name}
+        </Link>
+      ))}
+    </div>
+  );
 }
